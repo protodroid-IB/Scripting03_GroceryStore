be2e4a6f3fb5cc5a80038cf348637cfaf90c0847
Laurence <101215@student.sae.edu.au>
2018-08-24 13:53:22 +0800

    added credits

---
+++ GroceryStore/Assets/Credits.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Credits : MonoBehaviour
+{
+
+    private ScreenFader screenFader;
+
+    private bool fadeToMenu = false;
+
+    private void Start()
+    {
+        screenFader = GameObject.FindWithTag("ScreenFader").GetComponent<ScreenFader>();
+
+        screenFader.FadeFromBlack(1f);
+    }
+
+
+    public void MainMenu()
+    {
+        if (fadeToMenu == false)
+        {
+            screenFader.FadeToBlack(1f);
+            fadeToMenu = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (fadeToMenu == true && screenFader.FadeDone() == true)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
